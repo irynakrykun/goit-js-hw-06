f105c1596ea7e440050d5bd94552aa65dfa649cb
irynakrykun <ira_franyk@ukr.net>
2022-10-20 22:29:00 +0300

    task-5 add

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,9 @@
 const listItemEl = document.querySelectorAll('.item');
 
-console.log(`Number of categories: ${listItemEl.length}`);
+console.log("Number of categories:", listItemEl.length);
 
 for (let i = 0; i < listItemEl.length; i += 1){
-    console.log(`Category: ${listItemEl[i].querySelector('h2').textContent}`)
-  console.log(`Elements:  ${listItemEl[i].querySelectorAll('li').length}`)
+  console.log(`Category: ${listItemEl[i].querySelector('h2').textContent}`)
+  
+  console.log("Elements:", Number(listItemEl[i].querySelectorAll('li').length))
 }
---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const counterBtn = {
+  sub: document.querySelector("[data-action='increment']"),
+  add: document.querySelector("[data-action='decrement']"),
+  span: document.querySelector('#value'),
+  counter: document.querySelector('#counter '),
+};
+let counterValue = 0;
+const increment = () => {
+  counterValue += 1;
+
+  document.getElementById('value').textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+
+  document.getElementById('value').textContent = counterValue;
+};
+counterBtn.sub.addEventListener('click', increment);
+counterBtn.add.addEventListener('click', decrement);
